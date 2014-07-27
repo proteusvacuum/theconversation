60b47e7401664b20e90358b7b01b059cf05b4338
proteusvacuum <proteusvacuum@gmail.com>
2014-07-26 23:19:00 -0400

    some styling

--- static/js/home.js
+++ static/js/home.js
@@ -1,10 +1,9 @@
 $(function(){
 	var scrollPosition, $nav = $('nav#main-nav'), $fadesout = $(".fadesout");
 	var heroTextPosition = $('h1').offset().top;
-	
-	$(window).on("scroll", function(){
+	var checkNavBar = function(){
 		scrollPosition = $(window).scrollTop();
-		if (scrollPosition > heroTextPosition - $nav.height()){
+		if (scrollPosition > heroTextPosition - 2*$nav.height()){
 			$nav.find('.brand').fadeIn();
 			$fadesout.stop().animate({opacity: 0}, 200);
 			$nav.css({"background-color": "#fff"});
@@ -14,6 +13,28 @@ $(function(){
 			$fadesout.stop().animate({opacity: 1}, 200);
 			$nav.css({"background-color": "initial"});
 		}
+	};
+
+	// var checkImages = function(){
+	// 	$pis = $(".partner-image");
+	// 	if ( $pis.first().isInView() ){
+	// 		$pis.each(function(idx){
+	// 			var duration = 75;
+	// 			$(this).find("img").delay(duration*idx).animate({ opacity: 1 }, duration);
+	// 		});
+	// 	}
+	// 	else{
+	// 		$pis.each(function(idx){
+	// 			var duration = 25;
+	// 			$(this).find("img").css({ opacity: 0 })
+	// 		});
+	// 	}
+
+	// };
+
+	$(window).on("scroll", function(){
+		checkNavBar();
+		// checkImages();
 	});
 
 });
--- static/js/support_functions.js
+++ static/js/support_functions.js
@@ -1,5 +1,17 @@
 $(function() {
-  $("#twitter-login-button").on('click', function() {
-    window.location = '/auth/twitter/?next=' + encodeURIComponent(window.location.pathname + window.location.search);
-  });
+	$("#twitter-login-button").on('click', function() {
+		window.location = '/auth/twitter/?next=' + encodeURIComponent(window.location.pathname + window.location.search);
+	});
+});
+
+$.fn.extend({
+	isInView: function(){
+		var docViewTop = $(window).scrollTop();
+		var docViewBottom = docViewTop + $(window).height();
+		var $element = this.first();
+		var elemTop = $element.offset().top;
+		var elemBottom = elemTop + $element.height();
+		return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) &&
+			(elemBottom <= docViewBottom) &&  (elemTop >= docViewTop) );
+	}
 });
